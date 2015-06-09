242f6a66135f8ec0a3cc0f419b55253b13510f3f
CallMeAldy <aldrin.holmes20@gmail.com>
2015-06-09 12:13:19 +0200

    Such Changes. Much Wow.
    Signed-off-by: CallMeAldy <aldrin.holmes20@gmail.com>

--- products/minimal_falcon.mk
+++ products/minimal_falcon.mk
@@ -5,26 +5,26 @@ EnPRODUCT_PROPERTY_OVERRIDES += \
 $(call inherit-product, device/motorola/falcon/full_falcon.mk)
 
 # Inherit common product files.
-$(call inherit-product, vendor/xenonhd/products/common.mk)
+$(call inherit-product, vendor/minimal/products/common.mk)
 
 # Telephony
-$(call inherit-product, vendor/xenonhd/configs/telephony.mk)
+$(call inherit-product, vendor/minimal/configs/telephony.mk)
 
 # Inherit common build.prop overrides
--include vendor/xenonhd/products/common_versions.mk
+-include vendor/minimal/products/common_versions.mk
 
 # Copy hammerhead specific prebuilt files
 PRODUCT_COPY_FILES +=  \
-    vendor/xenonhd/proprietary/tuna/media/bootanimation.zip:system/media/bootanimation.zip \
-    vendor/xenonhd/proprietary/tuna/media/audio/notifications/Nexus.mp3:system/media/audio/notifications/Nexus.mp3 \
-    vendor/xenonhd/proprietary/common/media/LMprec_508.emd:system/media/LMprec_508.emd \
-    vendor/xenonhd/proprietary/common/media/PFFprec_600.emd:system/media/PFFprec_600.emd
+    vendor/minimal/proprietary/tuna/media/bootanimation.zip:system/media/bootanimation.zip \
+    vendor/minimal/proprietary/tuna/media/audio/notifications/Nexus.mp3:system/media/audio/notifications/Nexus.mp3 \
+    vendor/minimal/proprietary/common/media/LMprec_508.emd:system/media/LMprec_508.emd \
+    vendor/minimal/proprietary/common/media/PFFprec_600.emd:system/media/PFFprec_600.emd
 
 # Inherit drm blobs
--include vendor/xenonhd/products/common_drm.mk
+-include vendor/minimal/products/common_drm.mk
 
 # Setup device specific product configuration.
-PRODUCT_NAME := xenonhd_falcon
+PRODUCT_NAME := minimal_falcon
 PRODUCT_BRAND := motorola
 PRODUCT_DEVICE := falcon
 PRODUCT_MODEL := falcon
