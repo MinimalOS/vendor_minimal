a5fc6e9936598e5fc374d3229eb0fbc4c07c04ab
CallMeAldy <aldrin.holmes20@gmail.com>
2015-06-16 15:22:43 +0200

    Remove CM File Manager
    Signed-off-by: CallMeAldy <aldrin.holmes20@gmail.com>

--- products/common.mk
+++ products/common.mk
@@ -9,7 +9,6 @@ PRODUCT_PACKAGE_OVERLAYS += vendor/minimal/overlay/common
 PRODUCT_PACKAGES += \
     AudioFX \
     BluetoothExt \
-    CMFileManager \
     DeskClock \
     Development \
     Dialer \
