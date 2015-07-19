2d8417030eda895a765f8909b2f09095ad2284b3
dark-falcon <chinmay.kunkikar@gmail.com>
2015-07-19 18:30:34 +0530

    Update vendor *Update vendor in small pieces so that we don't break the source. **Deleted product makefiles for products that we don't build for. **Updated vendorsetup.sh
    Signed-off-by: dark-falcon <chinmay.kunkikar@gmail.com>

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -2,54 +2,12 @@ PRODUCT_MAKEFILES := \
 	$(LOCAL_DIR)/minimal_aries.mk \
 	$(LOCAL_DIR)/minimal_armani.mk \
 	$(LOCAL_DIR)/minimal_bacon.mk \
-	$(LOCAL_DIR)/minimal_crespo.mk \
 	$(LOCAL_DIR)/minimal_cancro.mk \
 	$(LOCAL_DIR)/minimal_condor.mk \
-	$(LOCAL_DIR)/minimal_d2spr.mk \
-	$(LOCAL_DIR)/minimal_d2bst.mk \
-	$(LOCAL_DIR)/minimal_d2att.mk \
- 	$(LOCAL_DIR)/minimal_d2tmo.mk \
- 	$(LOCAL_DIR)/minimal_d2usc.mk \
- 	$(LOCAL_DIR)/minimal_d2vzw.mk \
-	$(LOCAL_DIR)/minimal_e975.mk \
-	$(LOCAL_DIR)/minimal_endeavoru.mk \
 	$(LOCAL_DIR)/minimal_falcon.mk \
-	$(LOCAL_DIR)/minimal_find5.mk \
-	$(LOCAL_DIR)/minimal_find7.mk \
 	$(LOCAL_DIR)/minimal_flo.mk \
-	$(LOCAL_DIR)/minimal_galaxysmtd.mk \
-	$(LOCAL_DIR)/minimal_grouper.mk \
 	$(LOCAL_DIR)/minimal_hammerhead.mk \
-	$(LOCAL_DIR)/minimal_hammerheadcaf.mk \
-	$(LOCAL_DIR)/minimal_honami.mk \
-	$(LOCAL_DIR)/minimal_i605.mk \
-	$(LOCAL_DIR)/minimal_i9100.mk \
-	$(LOCAL_DIR)/minimal_i9100g.mk \
-	$(LOCAL_DIR)/minimal_i9300.mk \
-	$(LOCAL_DIR)/minimal_i9305.mk \
-	$(LOCAL_DIR)/minimal_jflte.mk \
-	$(LOCAL_DIR)/minimal_l900.mk \
 	$(LOCAL_DIR)/minimal_m7.mk \
-	$(LOCAL_DIR)/minimal_m8.mk \
-	$(LOCAL_DIR)/minimal_maguro.mk \
 	$(LOCAL_DIR)/minimal_mako.mk \
-	$(LOCAL_DIR)/minimal_mb886.mk \
-	$(LOCAL_DIR)/minimal_m7tmo.mk \
-	$(LOCAL_DIR)/minimal_n7000.mk \
-	$(LOCAL_DIR)/minimal_n7100.mk \
 	$(LOCAL_DIR)/minimal_pepper.mk \
-	$(LOCAL_DIR)/minimal_r950.mk \
 	$(LOCAL_DIR)/minimal_shamu.mk \
-	$(LOCAL_DIR)/minimal_t0lte.mk \
-	$(LOCAL_DIR)/minimal_t0lteatt.mk \
-	$(LOCAL_DIR)/minimal_t0ltetmo.mk \
-	$(LOCAL_DIR)/minimal_tilapia.mk \
-	$(LOCAL_DIR)/minimal_toro.mk \
-	$(LOCAL_DIR)/minimal_toroplus.mk \
-	$(LOCAL_DIR)/minimal_trltetmo.mk \
-	$(LOCAL_DIR)/minimal_trltexx.mk \
-	$(LOCAL_DIR)/minimal_victara.mk \
-	$(LOCAL_DIR)/minimal_vigor.mk \
-	$(LOCAL_DIR)/minimal_xt925.mk \
-	$(LOCAL_DIR)/minimal_xt926.mk \
-	$(LOCAL_DIR)/minimal_xt907.mk
