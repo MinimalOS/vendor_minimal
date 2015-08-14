c15dbc4ebc1535ad6ba0453c805ab3053248c492
George G <kreach3r@users.noreply.github.com>
2015-08-14 15:11:49 +0300

    Condor: update some directories
    to match the "Vendor: Major Update" commit by @Chinmay

--- products/minimal_condor.mk
+++ products/minimal_condor.mk
@@ -5,20 +5,20 @@ PRODUCT_PROPERTY_OVERRIDES += \
 $(call inherit-product, device/motorola/condor/full_condor.mk)
 
 # Inherit common product files.
-$(call inherit-product, vendor/minimal/products/common.mk)
+$(call inherit-product, vendor/minimal/config/common.mk)
 
 # Telephony
-$(call inherit-product, vendor/minimal/configs/telephony.mk)
+$(call inherit-product, vendor/minimal/config/telephony.mk)
 
 # Inherit common build.prop overrides
--include vendor/minimal/products/common_versions.mk
+-include vendor/minimal/config/common_versions.mk
 
 # Copy condor specific prebuilt files
 PRODUCT_COPY_FILES +=  \
     vendor/minimal/prebuilt/common/bootanimation/bootanimation.zip:system/media/bootanimation.zip
 
 # Inherit drm blobs
--include vendor/minimal/products/common_drm.mk
+-include vendor/minimal/config/common_drm.mk
 
 # Setup device specific product configuration.
 PRODUCT_NAME := minimal_condor
