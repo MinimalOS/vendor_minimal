2de66fb9a7fdbe6d1aff441bb6bd84aac1d7446b
Chinmay <dark-falcon@users.noreply.github.com>
2015-10-03 01:13:49 +0530

    grouper: Remove calls for telephony config

--- products/minimal_grouper.mk
+++ products/minimal_grouper.mk
@@ -4,15 +4,11 @@ $(call inherit-product, device/asus/grouper/full_grouper.mk)
 # Inherit common product files.
 $(call inherit-product, vendor/minimal/config/common.mk)
 
-# Telephony
-$(call inherit-product, vendor/minimal/config/telephony.mk)
-
 # Inherit common build.prop overrides
 -include vendor/minimal/config/common_versions.mk
 
 # Copy grouper specific prebuilt files
-PRODUCT_COPY_FILES +=  \
-    vendor/minimal/prebuilt/common/bootanimation/bootanimation.zip:system/media/bootanimation.zip
+MINIMAL_BOOTANIMATION_VARIANT := 1280
 
 # Inherit drm blobs
 -include vendor/minimal/config/common_drm.mk
